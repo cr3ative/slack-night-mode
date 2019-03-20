01886edfbfedcb1373b94344ac196881938831b1
Paul Curry <github@cr3ation.co.uk>
2019-03-20 12:20:26 +0000

    Fuck it

--- install_black.sh
+++ install_black.sh
@@ -2,22 +2,16 @@
 #
 # Usage: chmod +x install_black.sh; ./install_black.sh
 #
-#get latest css
-echo "Grabbing latest CSS version..."
-curl -o /Applications/Slack.app/Contents/Resources/black.css https://raw.githack.com/laCour/slack-night-mode/master/css/black.css
 #make slack load css
-if [[ `grep 'filePath = "/Applications/Slack.app/Contents/Resources/black.css"' /Applications/Slack.app/Contents/Resources/app.asar.unpacked/src/static/ssb-interop.js | wc -l` -lt 1 ]]; then
-    echo 'Installed CSS loader.';
-echo 'document.addEventListener("DOMContentLoaded", function() {
-  var fs = require("fs"),
-  filePath = "/Applications/Slack.app/Contents/Resources/black.css";
-  fs.readFile(filePath, {encoding: "utf-8"}, function(err, data) {
-    if (!err) {
-      var css = document.createElement("style")
-      css.innerText = data;
-      document.getElementsByTagName("head")[0].appendChild(css);
+if [[ `grep 'raw/black.css' /Applications/Slack.app/Contents/Resources/app.asar.unpacked/src/static/ssb-interop.js | wc -l` -lt 1 ]]; then
+  echo 'Installed CSS loader.';
+  echo 'document.addEventListener("DOMContentLoaded", function() {
+  $.ajax({
+    url: "https://raw.githubusercontent.com/laCour/slack-night-mode/master/css/raw/black.css",
+    success: function(css) {
+      $("<style></style>").appendTo("head").html(css);
     }
-  })
-});' >> /Applications/Slack.app/Contents/Resources/app.asar.unpacked/src/static/ssb-interop.js;
+  });
+ });' >> /Applications/Slack.app/Contents/Resources/app.asar.unpacked/src/static/ssb-interop.js;
 else echo 'CSS loader already installed.';
 fi
